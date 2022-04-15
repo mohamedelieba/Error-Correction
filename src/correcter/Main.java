8edfbf7888a91f2e92c57db5e51b515e7cf92826
mohamedelieba <mohamed.eliba712@gmail.com>
2022-04-15 16:29:47 +0200

    bit-level error emulator

--- src/correcter/Main.java
+++ src/correcter/Main.java
@@ -1,58 +1,62 @@
 package correcter;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
+    static String inputFile = "./send.txt";
     static Random random = new Random();
-
-    public static String introduceErrors(String text) {
-        int j = 0;
-        String faultyText = "";
-        StringBuilder triplet;
-        for (int i = 1; i <= text.length(); i++) {
-            if (i % 3 == 0) {
-                triplet = new StringBuilder(text.substring(j, i));
-                triplet.setCharAt(random.nextInt(3), (char) (random.nextInt(122 - 97) + 97));
-                j = i;
-                faultyText += triplet;
+    public static char binaryToText(String binary) {
+        int decimal = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '1') {
+                decimal += Math.pow(2, (binary.length() - i - 1));
             }
         }
-        faultyText += text.substring(j);
-        return faultyText;
+        return (char) decimal;
     }
 
-    public static String correctingText(String str) {
-        int k = 0;
-        String correctText = "";
-        StringBuilder symbol = null;
-        for (int i = 1; i <= str.length(); i++) {
-            if (i % 3 == 0) {
-                symbol = new StringBuilder(str.substring(k, i));
-                if (symbol.charAt(0) == symbol.charAt(1))
-                    symbol.setCharAt(2, symbol.charAt(0));
-                else if (symbol.charAt(0) == symbol.charAt(2))
-                    symbol.setCharAt(1, symbol.charAt(0));
+    public static String textToBinary(String text) {
+        String faultyText="";
+        Integer numericValue;
+        int randomPosition = 0;
+        StringBuilder binary;
+        for (char ch : text.toCharArray()) {
+            numericValue = new Integer(ch);
+            binary = new StringBuilder(numericValue.toBinaryString(numericValue));
+            randomPosition = random.nextInt(binary.length());
+                if (binary.charAt(randomPosition) == '0')
+                    binary.setCharAt(randomPosition, '1');
                 else
-                    symbol.setCharAt(0, symbol.charAt(1));
-                k = i;
-                correctText += symbol.charAt(0);
-            }
+                    binary.setCharAt(randomPosition, '0');
+            faultyText+=binaryToText(binary.toString());
         }
-        return correctText;
+        return faultyText;
+    }
+    public static void introduceErrors(String fileName) throws IOException {
+        File file = new File(inputFile);
+
+        String text = new String(Files.readAllBytes(Paths.get(fileName)));
+        File output = new  File("received.txt");
+        output.createNewFile();
+        try(PrintWriter printWriter = new PrintWriter(output)){
+
+            printWriter.print(textToBinary(text));
+        }catch(IOException e){
+                e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        System.out.println(text);
-        String sample = "";
-        for (char ch : text.toCharArray()) {
-            sample += ch + "" + ch + "" + ch;
+
+        try {
+            introduceErrors(inputFile);
+        } catch (IOException e) {
+            System.out.println("Cannot read file: " + e.getMessage());
         }
-        System.out.println(sample);
-        String str = introduceErrors(sample);
-        System.out.println(str);
-        System.out.println(correctingText(str));
     }
 }
